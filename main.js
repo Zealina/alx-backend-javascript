1b3d147657ae3f086d1b88513e60bbcbbe69801c
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-05 01:27:41 +0100

    Modify Javascripts Objects II

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
 import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
 
-const employees = {
-    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-    ...createEmployeesObject('marketing', ['Sylvie'])
-};      
-
-const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
