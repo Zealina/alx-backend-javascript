867f05868400cb1664f2dfea6245c85e573b39a9
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-06 13:45:46 +0100

    Handle multiple promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,12 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const user = createUser();
-  const photo = uploadPhoto();
-
-  Promise.all([user, photo])
-    .then((values) => console.log(`${values[1].body} ${values[0].firstName} ${values[0].lastName}`))
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, user]) => {
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
     .catch(() => {
-      console.log('Signup system offline');
+      console.error('Signup system offline');
     });
 }
