40859502d9eaf1d0a9a88326bb3167e6a280994d
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-06 17:09:17 +0100

    Asynchronous function

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((values) => ({
+      photo: values[0],
+      user: values[1],
+    }))
+    .catch(() => ({
+      photo: null,
+      user: null,
+    }));
+}
