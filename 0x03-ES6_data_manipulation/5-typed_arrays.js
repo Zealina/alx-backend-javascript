450d295d7652cf606c66571a6989d8b68265b8a7
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-12 00:22:50 +0100

    Create typed array

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,8 +2,8 @@ export default function createInt8TypedArray(length, position, value) {
   const ArrayBuffer = new Uint8Array(length);
   try {
     ArrayBuffer[position] = value;
+    return new DataView(ArrayBuffer.buffer);
   } catch (_) {
     throw new Error('Position outside range');
   }
-  return new DataView(ArrayBuffer.buffer);
 }
