09145bd7349fd1965a326ea61e03f5c4706537db
Sebastine Hamilton <hamilsebastine@gmail.com>
2024-03-11 22:07:13 +0100

    Create object list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,6 @@
+export default function getListStudents() {
+  const stud1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const stud2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const stud3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return ([stud1, stud2, stud3]);
+}
